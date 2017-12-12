38a921bcec1b96e6e8bff1e73e75da9c075ae528
Yannick Utard <yannickutard@gmail.com>
2017-12-12 22:31:45 +0100

    add mutations and cross overs

--- ai.py
+++ ai.py
@@ -1,14 +1,11 @@
 from generation import Generation
 
 def main():
-    # Execute one Generation
     generation = Generation()
     while True:
         generation.execute()
         generation.keep_best_genomes()
-
-    # Remove the worst genomes until we have 4
-    # Cross over and random mutations until we got 12 genomes again
+        generation.mutations()
 
 if __name__ == '__main__':
     main()
--- generation.py
+++ generation.py
@@ -3,6 +3,8 @@ from network import Network
 from time import sleep
 import numpy as np
 import pykeyboard
+import random
+import copy
 
 class Generation:
     def __init__(self):
@@ -31,11 +33,38 @@ class Generation:
             genome.fitness = scanner.get_fitness()
 
     def keep_best_genomes(self):
-        self.__genomes.sort(key=lambda x: x.fitness, reverse=True)[:4]
+        self.__genomes.sort(key=lambda x: x.fitness, reverse=True)
+        self.__genomes = self.__genomes[:4]
         self.__best_genomes = self.__genomes[:]
 
-    def cross_over(self):
-        print('cross over')
+    def mutations(self):
+        while len(self.__genomes) < 10:
+            genome1 = random.choice(self.__best_genomes)
+            genome2 = random.choice(self.__best_genomes)
+            self.__genomes.append(self.mutate(self.cross_over(genome1, genome2)))
+        while len(self.__genomes) < 12:
+            genome = random.choice(self.__best_genomes)
+            self.__genomes.append(self.mutate(genome))
 
-    def mutate(self):
-        print('mutate')
+    def cross_over(self, genome1, genome2):
+        new_genome = copy.deepcopy(genome1)
+        other_genome = copy.deepcopy(genome2)
+        cut_location = int(len(new_genome.W1) * random.uniform(0, 1))
+        for i in range(cut_location):
+            new_genome.W1[i], other_genome.W1[i] = other_genome.W1[i], new_genome.W1[i]
+        cut_location = int(len(new_genome.W2) * random.uniform(0, 1))
+        for i in range(cut_location):
+            new_genome.W2[i], other_genome.W2[i] = other_genome.W2[i], new_genome.W2[i]
+        return new_genome
+
+    def __mutate_weights(self, weights):
+        if random.uniform(0, 1) < 0.2:
+            return weights * (random.uniform(0, 1) - 0.5) * 3 + (random.uniform(0, 1) - 0.5)
+        else:
+            return 0
+
+    def mutate(self, genome):
+        new_genome = copy.deepcopy(genome)
+        new_genome.W1 += self.__mutate_weights(new_genome.W1)
+        new_genome.W2 += self.__mutate_weights(new_genome.W2)
+        return new_genome
